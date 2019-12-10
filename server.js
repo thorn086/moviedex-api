1ce3edaf4dabe45c0b0d303e98f291b2d2b5eafd
Darren Rose <thorn086@gmail.com>
2019-12-10 08:39:48 -0600

    updated code error in auth

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(function validateBearerToken(req, res, next) {
     const apiToken = process.env.API_TOKEN2
     const authToken = req.get('Authorization')
     
-    if (authToken || authToken.split(' ')[1] !== apiToken){
+    if (!authToken || authToken.split(' ')[1] !== apiToken){
         return res.status(401).json({error: 'Unauthorized Access'})
     }
 
